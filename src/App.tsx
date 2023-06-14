05b82a0c94da23011657fd274a373c88ca993b8f
AllwinJWork <allwinjwork@gmail.com>
2023-06-14 13:28:05 +0100

    final designs for the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Homepage from './Components/HomePage';
+import AboutPage from './Components/AboutPage';
 
 function App() {
   return (
    <>
    <Homepage></Homepage>
+   <AboutPage></AboutPage>
    
    
    </>
--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -1,22 +1,29 @@
 import ReactPlayer from 'react-player';
 import React, { useRef } from 'react';
 import '../Components/HomePage.css';
-import homeBg from '../Components/Assets/crowd-1056764_1920.jpg'
-
+import homeBg from '../Components/Assets/crowd-1056764_1920.jpg';
+import { Link } from 'react-router-dom';
 
 
 const MyComponent: React.FC = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-      <img className='HomePage' src={homeBg} alt="My Image" />
-      <div className="textOverlay">
+    <div className="container">
+    <h1>The Author.</h1>
+      <div>
+        <img className='homePage' src={homeBg} alt="My Image" />
+        <div className="textOverlay">
           <h2>Be inspired</h2>
           <p>There is power in music.</p>
+          <h3>Muisc is the universal language of mankind</h3>
         </div>
       </div>
+      <div className="textUnderImage">
+        <p>Your musical journey starts here today. Click below to get started right now!</p>
+      </div>
+    </div>
     
   );
 }
+
 
 export default MyComponent;
