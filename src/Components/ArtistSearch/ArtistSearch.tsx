c729f5a7b3cd4e015228bc5fd83711324e5316f3
AllwinJWork <allwinjwork@gmail.com>
2023-07-06 12:45:03 +0100

    added minor details for design

--- src/Components/ArtistSearch/ArtistSearch.tsx
+++ src/Components/ArtistSearch/ArtistSearch.tsx
@@ -1,24 +1,31 @@
-import React, { useState } from 'react'
-import './ArtistSearch.css'
+import React, { useState } from 'react';
+import './ArtistSearch.css';
 
 interface ArtistSearchProps {
   setArtistSearch: (artist: string) => void;
- 
-}
-const ArtistSearch = ({ setArtistSearch}:ArtistSearchProps) => {
-    const [searchQuery, setSearchQuery] = useState('')
-    const onChangeHandler = (e:React.ChangeEvent<HTMLInputElement>)   => {
-        setSearchQuery(e.target.value)
-    }
-    const onSubmitHandler= () => {
-        setArtistSearch(searchQuery)
-    }
-  return (
-    <div>
-      <input type="text" onChange={onChangeHandler} />
-      <button onClick={onSubmitHandler}>Search</button>
-    </div>
-  );
 }
 
-export default ArtistSearch
+const ArtistSearch = ({ setArtistSearch }: ArtistSearchProps) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const onSubmitHandler = () => {
+    setArtistSearch(searchQuery);
+  };
+
+  return (
+    <div className="artist-search-container">
+      <h1 className="title">Artist Inspiration</h1>
+      <p className="description">We will help you find new artists.</p>
+      <div className="search-bar">
+        <input type="text" value={searchQuery} onChange={onChangeHandler} placeholder="Search for an artist" />
+        <button onClick={onSubmitHandler}>Search</button>
+      </div>
+    </div>
+  );
+};
+
+export default ArtistSearch;
--- src/hooks/useRelatedArtists/useRelatedArtists.ts
+++ src/hooks/useRelatedArtists/useRelatedArtists.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Artists } from "./Artists.types";
 
 const useRelatedArtists = (accessToken: string, artistId: string) => {
